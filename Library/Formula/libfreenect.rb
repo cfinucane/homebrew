529bf408ae5a0f10187869a26adbda19ab48d5e3
Cameron Finucane <cpf37@cornell.edu>
2013-09-25 21:01:48 -0400

    libfreenect: build python bindings as well

--- Library/Formula/libfreenect.rb
+++ Library/Formula/libfreenect.rb
@@ -11,15 +11,27 @@ class Libfreenect < Formula
 
   depends_on 'cmake' => :build
   depends_on 'libusb'
+  depends_on :python
 
   def install
     if build.universal?
       ENV.universal_binary
       ENV['CMAKE_OSX_ARCHITECTURES'] = Hardware::CPU.universal_archs.as_cmake_arch_flags
     end
+
     mkdir "build" do
       system "cmake", "..", *std_cmake_args
       system "make install"
+    end
+
+    ENV.append_to_cflags "-L#{Dir.getwd}/build/lib"
+
+    cd "wrappers/python" do
+      python do
+        system python, "setup.py",
+                       "install",
+                       "--prefix=#{prefix}"
+      end
     end
   end
 end
